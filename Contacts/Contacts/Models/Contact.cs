1bbbdab698640e127ae4ddc0e84f8478b1f92825
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-02 15:08:42 +0300

    added Contact

---
+++ Contacts/Contacts/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName => Name + Surname;
+        public int PhoneNumber { get; set; }
+        public string GroupBy => FullName;
+    }
+}
--- Contacts/Contacts/Program.cs
+++ Contacts/Contacts/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Contacts.Models;
 
 namespace Contacts
 {
@@ -6,7 +9,18 @@ namespace Contacts
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var dictionary = new Dictionary<string, Contact>();
+           dictionary.Add("d", new Contact() { FullName = "ddf", PhoneNumber = 2434545 });
+           dictionary.Add("a", new Contact() { FullName = "adsds", PhoneNumber = 243325 });
+           dictionary.Add("b", new Contact() { FullName = "bddssds", PhoneNumber = 2433224 });
+           dictionary.Add("c", new Contact() { FullName = "cccwcwwfdfdfdf", PhoneNumber = 2322343 });
+           var list = dictionary.Keys.ToList();
+           list.Sort();
+           foreach (var key in list)
+            {
+                var keys = dictionary[key];
+                Console.WriteLine(keys);
+            }
         }
     }
 }
