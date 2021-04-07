7d7b6b4df7309889c99e9e53c377832cdec2eb37
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-07 19:16:18 +0300

    added FileService

---
+++ Contacts/Contacts/Services/FileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Services
+{
+    public class FileService
+    {
+        public string ReadAllText(string path) => File.ReadAllText(path);
+    }
+}
