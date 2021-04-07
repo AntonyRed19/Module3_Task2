2d30e2b59df24bb3048e2f3e6de102b32ea89c18
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-07 19:30:36 +0300

    added ConfigService

--- Contacts/Contacts/Services/ConfigService.cs
+++ Contacts/Contacts/Services/ConfigService.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contacts.Configs;
+using Contacts.Services.Abstractions;
 
 namespace Contacts.Services
 {
-    public class ConfigService
+    public class ConfigService : IConfigService
     {
+        private readonly string _filePath = "config.json";
+        private readonly IFileService _fileService;
+        private readonly LangConfig _langConfig;
+
+        public ConfigService(IFileService fileService)
+        {
+            _fileService = fileService;
+
+            var config = GetConfig();
+            _langConfig = config.Langs;
+        }
+
+        public LangConfig LangConfig => _langConfig;
+
+        private Config.Config GetConfig()
+        {
+            var json = _fileService.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<Config.Config>(json);
+        }
     }
 }
